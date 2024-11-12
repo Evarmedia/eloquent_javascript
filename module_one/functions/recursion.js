487a9c13c1046f12c2f87bd4d551518258669c5d
Evarmedia <mishakmanuel@gmail.com>
2024-11-12 21:04:11 +0100

    fixs

--- module_one/functions/recursion.js
+++ module_one/functions/recursion.js
@@ -10,10 +10,11 @@ For any other number N, its evenness is the same as N - 2.
 const isEven = (n) => {
     if (n === 0) return true
     if (n === 1) return false
-    if (n < 0) return "Please Enter A Real Number" // to correct the case of negative numbers causing infinite recursion
+    if (n < 0) return isEven(-n); // to correct the case of negative numbers causing infinite recursion
     else if (n % 2 === 0) return true
     else isEven(n-2)
     return false
 }
 
 console.log(isEven(50));
+console.log(isEven(-1));
